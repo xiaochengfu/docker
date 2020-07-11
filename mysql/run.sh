aa84aaa3320cdabc23f2b59e386340d831e10f5f
小程府 <1033426413@qq.com>
2020-07-11 10:50:06 +0800

    Update run.sh

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker run -v ~/docker-volumes/mysql/data:/var/lib/mysql \
- -v ~/docker-volumes/mysql/conf/my.cnf:/etc/mysql/my.cnf \
+ -v ~/docker-volumes/mysql/conf:/etc/mysql/conf.d \
  -e MYSQL_ROOT_PASSWORD=123456 \
  -p 3306:3306 \
  --restart=always \
